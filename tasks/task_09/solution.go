8478db2803f9517b2a72e7b36d7c00e51b3bc0a2
englandrecoil <nik.tereshchenko.982@gmail.com>
2026-02-27 18:04:52 +0300

    add task_09

--- tasks/task_09/solution.go
+++ tasks/task_09/solution.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"context"
+	"errors"
+	"sync"
 )
+
+type job[T any] struct {
+	id   int
+	data T
+}
 
 func ParallelMap[T any, R any](
 	ctx context.Context,
@@ -10,5 +17,68 @@ func ParallelMap[T any, R any](
 	in []T,
 	fn func(context.Context, T) (R, error),
 ) ([]R, error) {
-	// TODO
+	out := make([]R, len(in))
+
+	if workers <= 0 {
+		return out, errors.New("invalid number of workers")
+	}
+	if len(in) == 0 {
+		return out, nil
+	}
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	var wg sync.WaitGroup
+	wg.Add(workers)
+	jobs := make(chan job[T], len(in))
+
+	go func() {
+		defer close(jobs)
+		for i, v := range in {
+			job := job[T]{
+				id:   i,
+				data: v,
+			}
+			select {
+			case <-ctx.Done():
+				return
+			case jobs <- job:
+			}
+		}
+	}()
+
+	var fnErr error
+	var mu sync.Mutex
+	for range workers {
+		go func() {
+			defer wg.Done()
+			for job := range jobs {
+				if ctx.Err() != nil {
+					return
+				}
+
+				res, err := fn(ctx, job.data)
+				if err != nil {
+					mu.Lock()
+					if fnErr == nil {
+						fnErr = err
+					}
+					mu.Unlock()
+					cancel()
+					return
+				}
+				out[job.id] = res
+			}
+		}()
+	}
+
+	wg.Wait()
+	if fnErr != nil {
+		return nil, fnErr
+	}
+	if ctx.Err() != nil {
+		return nil, ctx.Err()
+	}
+	return out, nil
 }
